cd9ad161d95e5a9ede76472591cff8fe253d23aa
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-10-08 06:45:33 +0300

    Added emails

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [phone_number, setPhone_number] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !subject || !message) {
+      toast.error("Email not sent ,Please fill in all fields", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } else {
+      emailjs
+        .sendForm(
+          "service_m33cclr",
+          "template_grjxnup",
+          form.current,
+          "uExk6rUQ-FXxJCyKf"
+        )
+        .then(
+          (result) => {
+            setName("");
+            setEmail("");
+            setSubject("");
+            setMessage("");
+
+            setTimeout(() => {
+              toast.success("Email Sent , I will get back to you soon", {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+              });
+            }, 1000);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
+  };
   return (
     <div className="bg-gradient-to-t from-trabg-transparent">
+      <ToastContainer />
       <section id="contact">
         <div className="container mx-auto p-4">
           <div className="text-center my-8">
@@ -14,13 +75,16 @@ const Contact = () => {
             </h3>
           </div>
           <div className="flex items-center justify-center my-20">
-            <form action="">
+            <form onSubmit={sendEmail} ref={form}>
               <div className="flex flex-col md:flex-row lg:gap-32">
                 <div className="p-4">
                   <label htmlFor="name">
                     <input
                       type="text"
+                      name="name"
                       required
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
                       placeholder="NAME"
                       className="py-3 placeholder-[#00000080]  border-b-2 border-[#00000080] focus:outline-none w-full bg-transparent"
                     />
@@ -30,6 +94,9 @@ const Contact = () => {
                   <label htmlFor="email">
                     <input
                       type="email"
+                      value={email}
+                      name="email"
+                      onChange={(e) => setEmail(e.target.value)}
                       required
                       placeholder="EMAIL"
                       className="py-3  placeholder-[#00000080] border-b-2 border-[#00000080] focus:outline-none w-full bg-transparent"
@@ -44,6 +111,10 @@ const Contact = () => {
                     <input
                       type="text"
                       placeholder="PHONE NUMBER"
+                      required
+                      value={phone_number}
+                      name="phone_number"
+                      onChange={(e) => setPhone_number(e.target.value)}
                       className="py-3 placeholder-[#00000080] border-b-2 border-[#00000080] focus:outline-none w-full bg-transparent"
                     />
                   </label>
@@ -52,6 +123,9 @@ const Contact = () => {
                   <label htmlFor="subject">
                     <input
                       type="text"
+                      value={subject}
+                      name="subject"
+                      onChange={(e) => setSubject(e.target.value)}
                       required
                       placeholder="SUBJECT"
                       className="py-3 placeholder-[#00000080] border-b-2 border-[#00000080] focus:outline-none w-full bg-transparent"
@@ -64,6 +138,8 @@ const Contact = () => {
                   <textarea
                     name="message"
                     id="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                     cols="50"
                     rows="1"
                     required
@@ -73,7 +149,10 @@ const Contact = () => {
                 </label>
               </div>
               <div className="p-4 text-center lg:mt-8">
-                <button className="bg-[#293241] text-white px-5 py-2 rounded-lg">
+                <button
+                  type="submit"
+                  className="bg-[#293241] text-white px-5 py-2 rounded-lg"
+                >
                   Submit
                 </button>
               </div>
